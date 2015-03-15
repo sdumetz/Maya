5a67a651ebd85935efb924e62117454866b4f8f7
Sebastien Dumetz <sebastien.dumetz@isen-lille.fr>
2015-03-15 10:07:50 +0100

    added capability for multiple images for the same bat

--- index.js
+++ index.js
@@ -7,64 +7,72 @@ var path = require("path")
 
 
 var model = new Model();
-server.listen(8080);
-console.log("server started on http://localhost:8080");
 
-
-app.use(express.static('public'));
-//app.use('/dist',express.static('dist'));
-
-
-/*************************
- * Model synchronisation *
- * **********************/
-io.on('connection', function (socket) {
-  socket.join('room');
-  socket.on('change', function (data) {
-    model.updateCell(data,function(err,changedData){
-      if(!err){
-        console.log("changedData:",changedData)
-        io.to('room').emit('update',{tile:changedData,ressources:model.ressources});
-      }else{
-        console.log("socket.on Change :"+err);
-      }
-    })
-  });
-  socket.on("import",function(data){
-    model.fetch(function(err,doc){
-      if(err){
-        console.log("model.fetch : "+err);
-      }
-      console.log("model fetch")
-      socket.emit("import",doc);
-    });
-  })
-
-  /******************************
-   * Game Coroutines            *
-   * ***************************/
-  var routine = function(){
-    //Reset ressources that must be.
-    model.ressources.habitant.stock = 0;
-    model.fetch(function(err,doc){
-      doc.cases.forEach(function(tile){
-        model.updateRessources(tile);
-      });
-      Object.keys(model.ressources).every(function(ressource){
-        if(ressource.stock < 0){
-          console.log("YOU LOOSE");
-          return false;
-        }else{
-          return true;
-        }
-      });
-      io.to("room").emit('routine',{
-        ressources:model.ressources
-      })
-      setTimeout(routine,10000);
-    });
+var init = function(){
+  if(!model.ready()){
+    setTimeout(init,50);
+    return false;
   }
-  routine();
+  server.listen(8080);
+  console.log("server started on http://localhost:8080");
 
 
-});
+  app.use(express.static('public'));
+  //app.use('/dist',express.static('dist'));
+
+
+  /*************************
+   * Model synchronisation *
+   * **********************/
+  io.on('connection', function (socket) {
+    socket.join('room');
+    socket.on('change', function (data) {
+      model.updateCell(data,function(err,changedData){
+        if(!err){
+          console.log("changedData:",changedData)
+          io.to('room').emit('update',{tile:changedData,ressources:model.ressources});
+        }else{
+          console.log("socket.on Change :"+err);
+        }
+      })
+    });
+    socket.on("import",function(data){
+      model.fetch(function(err,doc){
+        if(err){
+          console.log("model.fetch : "+err);
+        }
+        console.log("model fetch")
+        socket.emit("import",doc);
+      });
+    })
+
+    /******************************
+     * Game Coroutines            *
+     * ***************************/
+    var routine = function(){
+      //Reset ressources that must be.
+      model.ressources.habitant.stock = 0;
+      model.fetch(function(err,doc){
+        doc.cases.forEach(function(tile){
+          model.updateRessources(tile);
+        });
+        Object.keys(model.ressources).every(function(ressource){
+          if(ressource.stock < 0){
+            console.log("YOU LOOSE");
+            return false;
+          }else{
+            return true;
+          }
+        });
+        io.to("room").emit('routine',{
+          ressources:model.ressources
+        })
+        setTimeout(routine,10000);
+      });
+    }
+    routine();
+
+
+  });
+}
+init();
