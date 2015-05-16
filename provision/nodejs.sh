940c68e8b9848e6df672ab9eda76c79bf173dacb
Sebastien Dumetz <sebastien.dumetz@isen-lille.fr>
2015-05-16 09:02:30 +0200

    added globally installed node modules provisionning

--- provision/nodejs.sh
+++ provision/nodejs.sh
@@ -6,3 +6,8 @@ apt-get install -y python-software-properties python g++ make
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
 apt-get install -y nodejs
+
+#Install global modules
+npm install -g grunt-cli
+npm install -g bower
+npm install -g mocha
